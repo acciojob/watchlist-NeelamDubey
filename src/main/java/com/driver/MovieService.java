ec5af10389c28f8dc7908a61ba1e66be85c86fad
Neelam Dubey <ashidubey8208@gmail.com>
2023-05-29 11:12:19 +0530

    Second Commit

---
+++ src/main/java/com/driver/MovieService.java
@@ -0,0 +1,42 @@
+package com.driver;
+
+import java.util.List;
+
+public class MovieService {
+    MovieRepository movieRepository=new MovieRepository();
+    public void addMovie(Movie movie) {
+        movieRepository.addMovie(movie);
+    }
+
+    public void addDirector(Director director) {
+        movieRepository.addDirector(director);
+    }
+
+    public void addMovieDirectorPair(String movie, String director) {
+        movieRepository.addMovieDirectorPair(movie,director);
+    }
+
+    public Movie getMovieByName(String name) {
+       return movieRepository.getMovieByName(name);
+    }
+
+    public Director getDirectorByName(String name) {
+        return movieRepository.getDirectorByName(name);
+    }
+
+    public List<String> findAllMovies() {
+        return movieRepository.findAllMovies();
+    }
+
+    public void deleteDirectorByName(String name) {
+        movieRepository.deleteDirectorByName(name);
+    }
+
+    public void deleteAllDirectors() {
+        movieRepository.deleteAllDirectors();
+    }
+
+    public List<String> getMoviesByDirectorName(String name) {
+        return movieRepository.getMoviesByDirectorName(name);
+    }
+}
